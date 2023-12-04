78dfb99579fa62752d319e46cbce08be166378f5
Dreamer <okay1211@126.com>
2023-12-04 17:37:23 +0800

    update

--- api/index.py
+++ api/index.py
@@ -5,4 +5,4 @@ app = register_app()
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run('main_http:app', host='0.0.0.0', port=9088, reload=True)
+    uvicorn.run('index:app', host='0.0.0.0', port=9088, reload=True)
